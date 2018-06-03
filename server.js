b298fea957af88fc2d67a9e8f69312a1a810c620
github username <github email address>
2018-06-03 11:10:26 -0400

    Making request and response server fun

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express')
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello Express!")
+  // res.send("<h1>Hello Express!</h1>")
+  res.send({
+    name: "Drew",
+    likes: ["Singing", "Hiking", "Coding"]
+  })
 })
 
 app.listen(3000)
