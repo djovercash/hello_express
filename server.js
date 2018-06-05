95eaf1a0dddaa0f5a4e2c26cddfb3e8a5bd21c49
github username <github email address>
2018-06-05 15:52:27 -0400

    Created header partial

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express')
+const hbs = require('hbs')
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  // res.send("<h1>Hello Express!</h1>")
-  res.send({
-    name: "Drew",
-    likes: ["Singing", "Hiking", "Coding"]
+  res.render('home.hbs', {
+    pageTitle: "Home Page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: "Welcome!"
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send("<h2>About Page</h2>")
+  res.render('about.hbs', {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.get('/bad', (req, res) => {
